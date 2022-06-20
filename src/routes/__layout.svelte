d97e8c4dd0ab4f5116b7b5ba2fa56157a396c4ed
Hussain Jawad <45372216+HusJAW@users.noreply.github.com>
2022-06-20 23:48:08 +0100

    Svelte Layout testing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,13 @@
 <script>
+	import Layout from './../../.svelte-kit/runtime/components/layout.svelte';
 	import '../app.css';
+	let checked = true;
+	//handels route
 </script>
 
+<header>Layout</header>
+<input type="checkbox" bind:checked />
+
 <slot />
+
+<footer>footer</footer>
